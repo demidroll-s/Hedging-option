ac6b0b31f31cabd4da68c30ec270d9e135e0e066
demidroll-s <suntsov.da@phystech.edu>
2022-02-03 21:15:37 +0300

    change bsm

--- black_scholes_model.cpp
+++ black_scholes_model.cpp
@@ -6,9 +6,9 @@ using namespace std;
  
 Black_Scholes_model::Black_Scholes_model() :
     drift(0.0),
-    stockPrice(0.0),
+    stock_price(0.0),
     volatility(0.0),
-    riskFreeRate(0.0),
+    risk_free_rate(0.0),
     date(0.0) {
 }
  
@@ -22,7 +22,7 @@ vector<double> Black_Scholes_model::generate_price_path(double to_date, int n_st
  
 vector<double> Black_Scholes_model::generate_price_path(double to_date, int n_steps, double drift) const {
     vector<double> path(n_steps, 0.0);
-    vector<double> epsilon = randn(n_steps);
+    vector<double> epsilon = rand_n(n_steps);
     double dt = (to_date - date) / n_steps;
     double a = (drift - volatility * volatility * 0.5) * dt;
     double b = volatility * sqrt(dt);
@@ -31,7 +31,7 @@ vector<double> Black_Scholes_model::generate_price_path(double to_date, int n_st
         double d_log_S = a + b * epsilon[i];
         double log_S = current_log_S + d_log_S;
         path[i] = exp(log_S);
-        current_Log_S = log_S;
+        current_log_S = log_S;
     }
     return path;
 }
@@ -42,50 +42,26 @@ vector<double> Black_Scholes_model::generate_price_path(double to_date, int n_st
 //
 ////////////////////////////////
  
-void testRiskNeutralPricePath() {
+void test_risk_neutral_price_path() {
     rng("default");
  
-    BlackScholesModel bsm;
-    bsm.riskFreeRate = 0.05;
+    Black_Scholes_model bsm;
+    bsm.risk_free_rate = 0.05;
     bsm.volatility = 0.1;
-    bsm.stockPrice = 100.0;
+    bsm.stock_price = 100.0;
     bsm.date = 2.0;
  
-    int nPaths = 10000;
-    int nsteps = 5;
+    int n_paths = 10000;
+    int n_steps = 5;
     double maturity = 4.0;
-    vector<double> finalPrices(nPaths,0.0);
-    for (int i=0; i<nPaths; i++) {
-        vector<double> path =
-            bsm.generateRiskNeutralPricePath( maturity,
-                                              nsteps );
-        finalPrices[i] = path.back();
+    vector<double> final_prices(n_paths, 0.0);
+    for (int i = 0; i < n_paths; i++) {
+        vector<double> path = bsm.generate_risk_neutral_price_path(maturity, n_steps);
+        final_prices[i] = path.back();
     }
-    ASSERT_APPROX_EQUAL( mean( finalPrices ),
-        exp( bsm.riskFreeRate*2.0)*bsm.stockPrice, 0.5);
+    ASSERT_APPROX_EQUAL(mean(final_prices), exp(bsm.risk_free_rate * 2.0) * bsm.stock_price, 0.5);
 }
- 
-void testVisually() {
-    BlackScholesModel bsm;
-    bsm.riskFreeRate = 0.05;
-    bsm.volatility = 0.1;
-    bsm.stockPrice = 100.0;
-    bsm.date = 2.0;
- 
-    int nSteps = 1000;
-    double maturity = 4.0;
- 
-    vector<double> path = bsm.generatePricePath( maturity,
-                                                 nSteps );
-    double dt = (maturity-bsm.date)/nSteps;
-    vector<double> times = linspace(dt, maturity, nSteps );
-    plot("examplePricePath.html",
-         times,
-         path );
-}
- 
- 
-void testBlackScholesModel() {
-    TEST( testRiskNeutralPricePath );
-    TEST( testVisually );
+
+void test_Black_Scholes_model() {
+    TEST(test_risk_neutral_price_path);
 }
